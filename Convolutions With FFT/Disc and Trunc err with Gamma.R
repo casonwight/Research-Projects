207906ff5179f7952651de2e89ebcb861237c7ba
Cason Wight <cason.wight@gmail.com>
2019-09-24 10:33:14 -0600

    Update Disc and Trunc err with Gamma.R
    Changed spacing

--- Convolutions With FFT/Disc and Trunc err with Gamma.R
+++ Convolutions With FFT/Disc and Trunc err with Gamma.R
@@ -1,14 +1,9 @@
 gam <- function(N, A, numConvs, a, b){
   supp <- seq(from=0, to = A*2, length.out = N*2)
-  
   f <- c(0,diff(pgamma(supp,a,b)))
-  
   ft <- fft(f)
-  
   newf <- Re(fft(ft^numConvs, inverse = TRUE))/(N*2)
-  
   real <- pgamma(supp[1:N], a*numConvs,b)
-  
   low <- cumsum(newf[1:N])-cumsum(newf[(N+1):(2*N)])
   upp <- cumsum(newf[1:(N+2)])[-(1:2)]
   
@@ -17,13 +12,10 @@ gam <- function(N, A, numConvs, a, b){
   }
   
   min.error <- optimize(f, interval = c(0,1))$minimum
-  
   return(list(cbind(low, real, upp), min.error))
 }
 
-
 gam(N=2^3, A=2^6, numConvs = 3, a = 2, b = 3)[[2]]
-
 
 best.est2 <- matrix(NA, nrow = 8, ncol = 8)
 
@@ -34,4 +26,3 @@ for(i in 3:10){
 }
 
 best.est2
-
